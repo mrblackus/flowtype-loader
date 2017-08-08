56c364080519c7c85b9b4e125e1d8f2f5eefc6a9
Mathieu Savy <mathieu.savy@gmail.com>
2017-08-08 15:26:14 +0200

    emit instance of Error if webpack 2

--- index.js
+++ index.js
@@ -1,12 +1,12 @@
-const util = require('util')
+const util = require('util');
 
 module.exports = function (source) {
   this.cacheable();
 
-  const options = this.flowtypeLoaderCheckOptions;
-  const checkAll = options && options.checkAll;
+  const options     = this.flowtypeLoaderCheckOptions;
+  const checkAll    = options && options.checkAll;
   const failOnError = options && options.failOnError;
-  const callback = this.async();
+  const callback    = this.async();
 
   if (!checkAll) {
     const flowAnnotation = source.slice(0, 8);
@@ -18,7 +18,9 @@ module.exports = function (source) {
 
   this.flowtypeLoaderCheckContent(source, this.resourcePath, function (errors, options) {
     if (errors.length > 0) {
-      errors.map(this.emitError);
+      errors
+        .map(x => this.version === 2 ? new Error(x) : x)
+        .map(this.emitError);
       if (failOnError) {
         throw new Error('Module failed because of a Flow error.\n' + errors.join('\n'));
       }
